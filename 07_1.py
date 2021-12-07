b034956bff45eb379d65bab7fb154077d425f11c
Jonathan Ek <jonathan.ek@contextvision.se>
2021-12-07 08:32:11 +0100

    Day 7

---
+++ 07_1.py
@@ -0,0 +1,16 @@
+def main():
+    with open('input_07.txt', 'r') as fp:
+        data = [int(x) for x in fp.readlines()[0].split(',')]
+        d_min = min(data)
+        d_max = max(data)
+        distances = {}
+        for d in range(d_min, d_max+1):
+            tmp = 0
+            for c in data:
+                tmp += abs(d - c)
+            distances[d] = tmp
+        print(sorted(distances.items(), key=lambda x: x[1])[0][1])
+
+
+if __name__ == '__main__':
+    main()
---
+++ 07_2.py
@@ -0,0 +1,16 @@
+def main():
+    with open('input_07.txt', 'r') as fp:
+        data = [int(x) for x in fp.readlines()[0].split(',')]
+        d_min = min(data)
+        d_max = max(data)
+        distances = {}
+        for d in range(d_min, d_max+1):
+            tmp = 0
+            for c in data:
+                tmp += sum(range(abs(d - c)+1))
+            distances[d] = tmp
+        print(sorted(distances.items(), key=lambda x: x[1])[0][1])
+
+
+if __name__ == '__main__':
+    main()
