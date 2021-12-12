0bfb2bc2b1629523b057a519d1c9e48c025922ea
Jonathan Ek <jonathan@jonathananderson.se>
2021-12-12 09:23:27 +0100

    Day 12

--- 12_2.py
+++ 12_2.py
@@ -10,21 +10,23 @@ class Node:
         self.paths.append(node)
         
         
-def get_paths(history, current):
+def get_paths(history, current, prevent_dual=False):
     paths = []
     for n in current.paths:
+        used_dual = prevent_dual
         if n in history and n.small:
-            continue
+            used_dual = True
+            if prevent_dual or n.id == 'start':
+                continue
         if n.id == 'end':
             paths.append([*history, current, n])
             continue
-        paths += get_paths([*history, current], n)
+        paths += get_paths([*history, current], n, used_dual)
     return paths
 
 def main():
     with open('input/12.txt', 'r') as fp:
         data = [x.strip().split('-') for x in fp.readlines()]
-        print(data)
         lo = dict()
         nodes = set([d for x in data for d in x])
         for node in nodes:
@@ -35,7 +37,6 @@ def main():
             n1= lo[b]
             n0.add_path(n1)
             n1.add_path(n0)
-            
             
         start = lo['start']
         end = lo['end']
