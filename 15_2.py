9dc2fab74e41e8fb59e065ea3a9797ff0adaff4e
Jonathan Ek <jonathan.ek@contextvision.se>
2021-12-15 10:55:48 +0100

    Day 15

--- 15_2.py
+++ 15_2.py
@@ -11,14 +11,19 @@ class Node:
 
 def main():
     with open('input/15.txt', 'r') as fp:
-        data = [[Node((x, y), int(val)) for x, val in enumerate(row.strip())] for y, row in enumerate(fp.readlines())]
+        data = [[int(val) for val in row.strip()] for row in fp.readlines()]
+        big_data = [[(data[y][x] + (xit + yit)) % 9 or 9 for xit in range(5) for x in range(len(data[0]))] for yit in range(5) for y in range(len(data)) ]
+        data = [[Node((x, y), int(val)) for x, val in enumerate(row)] for y, row in enumerate(big_data)]
         nodes = [x for y in data for x in y]
+        total = len(nodes)
         x_max = len(data[0]) - 1
         y_max = len(data) - 1
         initial = data[0][0]
         end = data[y_max][x_max]
         initial.distance = 0
         current = initial
+        i = 0
+        unvisited_with_value = []
         while True:
             if current == end:
                 print(current.distance)
@@ -32,8 +37,13 @@ def main():
             for n in neighbours:
                 if n.distance is None or n.distance > (current.distance + n.value):
                     n.distance = current.distance + n.value
+                    unvisited_with_value.append(n)
             current.visited = True
-            sorted_nodes = sorted([x for x in nodes if not x.visited and x.distance is not None], key=lambda x: x.distance)
+            unvisited_with_value = [x for x in unvisited_with_value if not x.visited]
+            sorted_nodes = sorted([x for x in unvisited_with_value], key=lambda x: x.distance)
+            i += 1
+            if i % 400 == 100:
+                print(len([x for x in nodes if x.visited]) / total)
             current = sorted_nodes[0]
 
 
