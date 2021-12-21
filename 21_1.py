b3b50723c0976691b326df0cf0025d2f2bdfc28b
Jonathan Ek <jonathan@paaventyr.se>
2021-12-21 08:49:51 +0100

    Day 21 part 1

--- 21_1.py
+++ 21_1.py
@@ -1,6 +1,28 @@
 def main():
     with open('input/21.txt', 'r') as fp:
-        data = [int(x) for x in fp.readlines()]
+        pos = [int(x.split(': ')[1]) for x in fp.readlines()]
+        data = [0, 0]
+        current_player = 0
+        current_dies = []
+        die_rolls = 0
+        while True:
+            for x in range(100):
+                die_rolls += 1
+                val = x+1
+                current_dies.append(val)
+                if len(current_dies) == 3:
+                    pos[current_player] += sum(current_dies)
+                    pos[current_player] = pos[current_player] % 10
+                    if pos[current_player] == 0:
+                        pos[current_player] += 10
+                    data[current_player] += pos[current_player]
+                    if data[current_player] >= 1000:
+                        loosing_player = 1 if current_player == 0 else 0
+                        print(data[loosing_player] * die_rolls)
+                        return
+
+                    current_player = 1 if current_player == 0 else 0
+                    current_dies = []
 
 
 if __name__ == '__main__':
